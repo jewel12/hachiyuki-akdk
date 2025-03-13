6b0c63238e815763a900ca5e566e116dbf5de537
MasamichiIdeue <ca00444@cartahd.com>
2025-03-14 02:00:24 +0900

    init

---
+++ qmk_firmware/keyboards/hachiyuki/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define EE_HANDS
+#define VIAL_KEYBOARD_UID {0x1a, 0x54, 0xd5, 0x60, 0x3e, 0xcf, 0x0a, 0x1f}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
